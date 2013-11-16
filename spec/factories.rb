fd9af71d78ce415fff4bff66cfd6628955512682
solostyle <spam@solostyle.net>
2013-11-16 02:11:48 -0600

    added gravatar image to users profile page

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
     factory :user do
         name        "archana sriram"
-        email       "archana.sriram@careerbuilder.com"
+        email       "spam@solostyle.net"
         password    "foobar"
         password_confirmation   "foobar"
     end
