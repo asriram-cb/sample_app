34e135de703d33db0e9fc865f023587d3e8778a1
solostyle <spam@solostyle.net>
2013-11-16 01:40:39 -0600

    added basic user profile page and tests

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name        "archana sriram"
+        email       "archana.sriram@careerbuilder.com"
+        password    "foobar"
+        password_confirmation   "foobar"
+    end
+end
