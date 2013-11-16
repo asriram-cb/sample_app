6a6e90fb21abbf2d193095797a25cdf313fb7a9c
solostyle <spam@solostyle.net>
2013-11-16 02:44:09 -0600

    disallow invalid user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,22 @@
 class UsersController < ApplicationController
-    def signup
-    end
-
     def index
     end
 
     def show
         @user = User.find(params[:id])
     end
-    
+
     def new
+        @user = User.new
     end
 
     def create
+        @user = User.new(user_params)     # Not the final implementation!
+        if @user.save
+            # Handle a successful save
+        else
+            render 'new'
+        end
     end
 
     def edit
@@ -22,5 +26,11 @@ class UsersController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
