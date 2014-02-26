7f2b41a8a1e31cc68d43867d4c16279a8933fc2c
solostyle <spam@solostyle.net>
2014-02-25 20:39:31 -0600

    final commit for rails tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
     resources :users
     resources :sessions, only: [:new, :create, :destroy] # index, edit, update, show
-    root  'static_pages#home'    #fuckin cool
-    get 'こんにちは', to: 'static_pages#help'
+    root  'static_pages#home'
+    get 'こんにちは', to: 'static_pages#help' #fuckin cool
     match '/help',		to: 'static_pages#help',    via: 'get'
     match '/about', 	to: 'static_pages#about',   via: 'get'
     match '/contact',	to: 'static_pages#contact', via: 'get'
